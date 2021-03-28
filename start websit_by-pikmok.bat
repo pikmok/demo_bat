2ffe9d14b557ce61e99917f395422238c0f5f542
pikmok <81107617+pikmok@users.noreply.github.com>
2021-03-28 19:12:56 +0300

    Add files via upload

---
+++ start websit_by-pikmok.bat
@@ -0,0 +1,19 @@
+title start_websit_by-pikmok 
+@echo off
+setlocal
+:PROMPT
+SET /P AREYOUSURE=Are you sure to start web? (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+set /p web="Enter web: "
+
+start %web%
+start %web%
+start %web%
+start %web%
+start %web%
+start %web%
+:END
+endlocal
+#ping %ip_addr% -l 65500 -w 1 -n 5	
+
